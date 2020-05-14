c11bc2528f1b734db6228bc3e938677145ad1b3b
Alexander <caravaev_alexand@mail.ru>
2020-05-15 01:30:57 +0700

    Write a code to calculate the perimeter and area

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+#define pi 3, 14
+
+double Perimeter(double radius)
+{
+    return 2 * pi * radius;
+}
+
+double Area(double radius)
+{
+    return pi * radius * radius;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    double x, y, r;
+
+    cout << "Введите координаты и радиус круга:\n";
+    cout << "X: ";
+    cin >> x;
+    cout << "Y: ";
+    cin >> y;
+    cout << "R: ";
+    cin >> r;
+
+    if (r < 0) {
+        cout << "\nРадиус должен быть положительным\n"
+             << "\nНажмите любую клавишу чтобы выйти\n";
+        cin.get();
+        cin.get();
+        return 0;
+    }
+
+    cout << "\nCircle(" << x << " " << y << ", " << r << ")\n";
+    cout << "Area: " << Area(r) << endl;
+    cout << "Perimeter: " << Perimeter(r) << endl;
+
+    cout << "\nНажмите любую клавишу чтобы выйти\n";
+    cin.get();
+    cin.get();
+    return 0;
+}
